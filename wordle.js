86341ac4f07bcbd386303dd359b0cfb50edf4d8e
Madeleine <madeleine.dahlqvist@lerniastudent.se>
2024-03-17 00:34:30 +0100

    Add algorithm and fix bugs

--- wordle.js
+++ wordle.js
@@ -1,9 +1,20 @@
 export default function wordle(str, str2) {
+const answer = [];
+const correctLetters = {};
   for (let i = 0; i < str.length; i++) {
+    let result = "incorrect";
     for (let j = 0; j < str2.length; j++) {
-      if (str.charAt(i) === str2.charAt(j)) {
-        console.log("correct");
+      if (str.charAt(i) === str2.charAt(j) && i === j) {
+        result = "correct";
+        correctLetters[str.charAt(i)] = true;
       }
     }
+    answer.push({letter: str.charAt(i), result});
   }
+  for (let i = 0; i < str.length; i++) {
+    if (str2.includes(str.charAt(i)) && correctLetters[str.charAt(i)] !== true) {
+      answer[i].result = "misplaced";
+    }
+  }
+  return answer;
 }
--- wordle.spec.js
+++ wordle.spec.js
@@ -2,10 +2,18 @@ import { describe, expect, it } from "@jest/globals";
 
 import wordle from "./wordle.js";
 
+/* Teststrategin:
+1. Skapa testerna
+2. Se till att testerna hittar problem i algoritmen
+3. Skapa algoritmen
+4. Kör testerna och försök lösa problemen som testen visar att algoritmen har
+5. När alla test har lyckats vet jag att algoritmen är korrekt
+*/
+
 describe("wordle()", () => {
   it("find letter in correct place but with the wrong word", () => {
     const output = wordle("money", "local");
-    expect(output).toBe([
+    expect(output).toEqual([
       { letter: "m", result: "incorrect" },
       { letter: "o", result: "correct" },
       { letter: "n", result: "incorrect" },
@@ -15,7 +23,7 @@ describe("wordle()", () => {
   });
   it("find the correct word", () => {
     const output = wordle("local", "local");
-    expect(output).toBe([
+    expect(output).toEqual([
       { letter: "l", result: "correct" },
       { letter: "o", result: "correct" },
       { letter: "c", result: "correct" },
@@ -25,7 +33,7 @@ describe("wordle()", () => {
   });
   it("find misplaced letters with the wrong word", () => {
     const output = wordle("crash", "local");
-    expect(output).toBe([
+    expect(output).toEqual([
       { letter: "c", result: "misplaced" },
       { letter: "r", result: "incorrect" },
       { letter: "a", result: "misplaced" },
@@ -35,12 +43,22 @@ describe("wordle()", () => {
   });
   it("find misplaced and correct letters with the wrong word", () => {
     const output = wordle("label", "local");
-    expect(output).toBe([
+    expect(output).toEqual([
       { letter: "l", result: "correct" },
       { letter: "a", result: "misplaced" },
       { letter: "b", result: "incorrect" },
       { letter: "e", result: "incorrect" },
       { letter: "l", result: "correct" },
+    ]);
+  });
+  it("find misplaced and correct letters with the wrong word", () => {
+    const output = wordle("hallå", "cykla");
+    expect(output).toEqual([
+      { letter: "h", result: "incorrect" },
+      { letter: "a", result: "misplaced" },
+      { letter: "l", result: "incorrect" },
+      { letter: "l", result: "correct" },
+      { letter: "å", result: "incorrect" },
     ]);
   });
 });
